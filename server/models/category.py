41fb30ba57b0c21cc3e249bd9a4fa3e99f0fc380
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-06-25 19:38:21 -0400

    Category model complete

--- server/models/category.py
+++ server/models/category.py
@@ -4,4 +4,23 @@ from sqlalchemy.orm import validates
 from config import db
 
 class Category(db.Model):
-    """Category model for categorizing products and transactions."""  
+    """Category model for categorizing products and transactions."""
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    __table_args__ = (
+        CheckConstraint("length(name) >= 1", name="name_min_length"),
+        UniqueConstraint("user_id", "name", name="unique_category_per_user"),
+    )
+
+    @validates("name")
+    def validate_name(self, key, value):
+        if not isinstance(value, str):
+            raise ValueError(f"{key} must be a string.")
+        stripped_value = value.strip().lower()
+        if len(stripped_value) < 1 or len(stripped_value) > 50:
+            raise ValueError(f"{key} must be between 1 and 50 characters long.")
+        return stripped_value
