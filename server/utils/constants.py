1ca99902b59dcfb8d62983b37befcd42e6c76327
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-07-12 12:22:06 -0400

    New constants added

--- server/utils/constants.py
+++ server/utils/constants.py
@@ -9,3 +9,5 @@ SKU_REGEX = re.compile(r"^[A-Z0-9_-]+$")
 
 PRODUCT_CREATION_ALLOWED_KEYS = {'name', 'sku', 'price', 'total_units', 'min_stock', 'max_stock', 'category_id', 'user_id'}
 PRODUCT_UPDATE_ALLOWED_KEYS = {'name', 'sku', 'price', 'min_stock', 'max_stock', 'category_id'}
+CATEGORY_CREATION_ALLOWED_KEYS = {'name', 'user_id'}
+CATEGORY_UPDATE_ALLOWED_KEYS = {'name'}
