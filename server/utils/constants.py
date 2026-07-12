b6599de0294a635844d6988e479f45af5388fcdd
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-07-12 11:57:33 -0400

    Constants added

--- server/utils/constants.py
+++ server/utils/constants.py
@@ -6,3 +6,6 @@ TRANSACTION_TYPES = ("Restock", "Sell")
 USERNAME_REGEX = re.compile(r"^[a-z0-9_]+$")
 PASSWORD_REGEX = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$")
 SKU_REGEX = re.compile(r"^[A-Z0-9_-]+$")
+
+PRODUCT_CREATION_ALLOWED_KEYS = {'name', 'sku', 'price', 'total_units', 'min_stock', 'max_stock', 'category_id', 'user_id'}
+PRODUCT_UPDATE_ALLOWED_KEYS = {'name', 'sku', 'price', 'min_stock', 'max_stock', 'category_id'}
