ef731a158192d05c63db7e9351a045eff9b0ab85
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-07-12 20:57:47 -0400

    Non-existent import deleted

--- server/schemas/__init__.py
+++ server/schemas/__init__.py
@@ -1,4 +1,4 @@
 from .user_schemas import UserSchema, CreateUserSchema, UpdateUserSchema
 from .category_schemas import CategorySchema, CreateCategorySchema, UpdateCategorySchema
 from .product_schemas import ProductSchema, CreateProductSchema, UpdateProductSchema
-from .transaction_schemas import TransactionSchema, CreateTransactionSchema, UpdateTransactionSchema
+from .transaction_schemas import TransactionSchema, CreateTransactionSchema
