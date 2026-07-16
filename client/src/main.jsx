b6ebe78b49947b5d28850f802223bb6e89a9f89d
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-07-16 10:59:05 -0400

    ProductContext done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,14 +5,17 @@ import App from './App.jsx'
 import { ThemeProvider } from './contexts/ThemeContext.jsx'
 import { UserProvider } from './contexts/UserContext.jsx'
 import { CategoryProvider } from './contexts/CategoryContext.jsx'
+import { ProductProvider } from './contexts/ProductContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserProvider>
       <CategoryProvider>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
+        <ProductProvider>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </ProductProvider>
       </CategoryProvider>
     </UserProvider>
   </StrictMode>,
