999799502e98fb549c2981f98cec82295c1a419f
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2026-07-17 10:02:08 -0400

    Home done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,36 @@
+import { Container, Box } from "@mui/material";
+import { Outlet, useNavigate } from "react-router-dom";
 
-
+import Header from "../components/Header";
+import { useUserContext } from "../contexts/UserContext";
 
 export default function Home() {
+    const navigate = useNavigate();
+    const { logout } = useUserContext();
+
+    const handleLogout = async () => {
+        await logout();
+        navigate("/login", { replace: true });
+    };
+
     return (
-        <p>Home</p>
-    )
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                width: "100%",
+            }}
+        >
+            <Container
+                maxWidth="lg"
+                sx={{
+                    py: 3,
+                    px: { xs: 2, md: 3 },
+                }}
+            >
+                <Header onLogout={handleLogout} />
+                <Outlet />
+            </Container>
+        </Box>
+    );
 }
